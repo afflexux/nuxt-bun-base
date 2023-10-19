e73c9d350a44ad5f7a151cc8871b8a0977471f58
afflexux <afflexux@gmail.com>
2023-10-19 20:09:26 +0100

    added image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,3 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1 class="py-4 text-center text-2xl text-green-600">Hello Nuxt3</h1>
+    <NuxtImg
+      src="https://images.pexels.com/photos/5567049/pexels-photo-5567049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    />
   </div>
 </template>
 
