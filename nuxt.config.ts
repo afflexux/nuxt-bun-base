8269321d2cf93f304a1f2ba91748565451bcf232
afflexux <35804265+afflexux@users.noreply.github.com>
2024-03-02 14:54:01 +0000

    replaced font import with module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,10 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@nuxtjs/google-fonts'],
+  googleFonts: {
+    display: 'swap',
+    families: {
+      Inter: true,
+      //   'Plus+Jakarta+Sans': true,
+    },
+  },
 })
