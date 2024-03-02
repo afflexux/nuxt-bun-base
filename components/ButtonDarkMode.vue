5a76f1964f0974711864a770de9a8db1a43dcfea
afflexux <35804265+afflexux@users.noreply.github.com>
2024-03-02 19:25:42 +0000

    added dark mode button

---
+++ components/ButtonDarkMode.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex flex-row items-center justify-center">
+    <Icon
+      v-show="colorMode.preference === 'light'"
+      name="bx:sun"
+      class="theme-icon text-typography_primary_light dark:text-typography_primary_dark"
+      width="28"
+      height="28"
+      @click="toggleTheme"
+    />
+    <Icon
+      v-show="colorMode.preference === 'dark'"
+      name="bx:bxs-sun"
+      class="theme-icon text-typography_primary_light dark:text-typography_primary_dark"
+      width="28"
+      height="28"
+      @click="toggleTheme"
+    />
+  </div>
+</template>
+
+<script setup>
+const colorMode = useColorMode()
+const toggleTheme = () => {
+  colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark'
+}
+</script>
+
+<style scoped>
+.theme-icon {
+  @apply w-7 h-7 cursor-pointer transition-transform duration-100;
+}
+.theme-icon:hover {
+  @apply scale-125;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,27 +3,8 @@
     <!-- <NuxtImg
       src="https://images.pexels.com/photos/5567049/pexels-photo-5567049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     /> -->
-    <UCard class="max-w-screen-md mx-auto">
-      <template #header>
-        <h1 class="font-bold text-green-600 text-lg">This is the card title</h1>
-      </template>
-      <UAvatar
-        src="https://avatars.githubusercontent.com/u/739984?v=4"
-        alt="Avatar"
-      />
-      <p class="text-gray-600">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-        Necessitatibus, ipsum quisquam. Rerum architecto non velit consequuntur
-        reprehenderit dolorem officiis suscipit error mollitia ab illo a iure
-        explicabo, quam quod adipisci!
-      </p>
-
-      <template #footer>
-        <h2 class="text-sm text-green-800 font-semibold">
-          This is the card footer
-        </h2>
-      </template>
-    </UCard>
+    <ButtonDarkMode class="pb-3" />
+    <TestCard />
   </div>
 </template>
 
