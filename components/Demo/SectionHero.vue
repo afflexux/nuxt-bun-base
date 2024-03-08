ebc9cec12a324b70b860836eb9ba4fc64b90e37a
afflexux <35804265+afflexux@users.noreply.github.com>
2024-03-08 21:30:55 +0000

    moved into demo folder

---
+++ components/Demo/SectionHero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <DemoUiSplit v-show="colorMode.value === 'light'" />
+    <DemoUiSplitDark v-show="colorMode.value === 'dark'" />
+    <DemoUiFeature v-show="colorMode.value === 'light'" />
+    <DemoUiFeatureDark v-show="colorMode.value === 'dark'" />
+    <DemoUiFooter v-show="colorMode.value === 'light'" />
+    <DemoUiFooterDark v-show="colorMode.value === 'dark'" />
+  </div>
+</template>
+
+<script setup>
+const colorMode = useColorMode()
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
     <ButtonDarkMode class="py-3" />
     <!-- <TestCard /> -->
     <!-- Uncomment for dark mode v-show example Tailwind UI -->
-    <SectionHero />
+    <DemoSectionHero />
   </div>
 </template>
 
